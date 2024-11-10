3ef9b8225735cbe4d44bbc3b286dca628f4cfccf
Mortikst <valerkar16@gmail.com>
2024-11-10 18:50:04 +0200

    added lesson_03

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("This is main class");
+        System.out.println("This is main class ");
     }
 }
