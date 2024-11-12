d3e5be70f0fd4d6791fa37195bd696b98118337e
Mortikst <valerkar16@gmail.com>
2024-11-12 13:42:59 +0200

    finish lesson_11

---
+++ src/lesson_11/MathOperations.java
@@ -0,0 +1,44 @@
+package src.lesson_11;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        //Прибавление
+        int sum = x + y;
+        //System.out.println(sum);
+
+        //Вычетание
+        int diff = x - y;
+        //System.out.println(diff);
+
+        //умоножение
+        int multiply = x * y;
+        //System.out.println(multiply);
+
+        //Деление
+        y = 3;
+        double divide = (double) x / y;
+        //System.out.println(divide);
+
+        //Возведение в степень
+        int ext = x * x;
+        //System.out.println(ext);
+
+        //Иткремент
+        //x = x + 1;
+        //x++;
+//        System.out.println(x);
+//
+        //Сокращенная форма записи
+//        x += 10;
+//        x *= 10;
+//        x /= 2;
+
+        //Декремент
+//        x = x -1;
+//          x--;
+//          System.out.println(x);
+    }
+}
